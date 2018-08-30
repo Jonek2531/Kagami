f552ec288cedba633e55ac7ef0eeeca3eb609a62
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2018-08-30 18:56:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,24 +4,6 @@ const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true})
 bot.commands = new Discord.Collection();
 
-fs.readdir("./commands/", (err, files) => {
-
- if(err) console.log(err);
-
- let jsfile = files.filter(f => f.split(".").pop() === "js")
- if(jsfile.length <= 0){
-   console/log("Nie moge znalesc komendy");
-   return;
- }
-})
-
-fs.readdir("./commands/", (err, files) =>{
-  if(err) console.log(err);
-  let jsfile = files.filter(f => f.split(".").pop() === "js");
-  if(jsfile.length <= 0){
-    console.log("Nie ma takiej komendy.");
-    return;
-  }
 
   jsfile.forEach((f, i) =>{
     let props = require(`./commands/${f}`);
