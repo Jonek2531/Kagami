2f2f8877abbd347919c76a8fdb5485f8dc008928
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2018-08-30 19:35:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -15,13 +15,6 @@ fs.readdir("./commands/", (err, files) => {
  }
 })
 
-fs.readdir("./commands/", (err, files) =>{
-  if(err) console.log(err);
-  let jsfile = files.filter(f => f.split(".").pop() === "js");
-  if(jsfile.length <= 0){
-    console.log("Nie ma takiej komendy.");
-    return;
-  }
 
   jsfile.forEach((f, i) =>{
     let props = require(`./commands/${f}`);
