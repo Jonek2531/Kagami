8396d7591006c6ce59450cc033fd5fffe6ec583f
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2018-08-31 19:33:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -373,6 +373,36 @@ if(cmd === `${prefix}trump`){
   message.channel.send(embed)
 }
 	
+	if(cmd === `${prefix}zapal`){
+	  message.channel.send('**ja PAAAAAAAAALE**').then(async msg => {
+  setTimeout(() => {
+    msg.edit('🚬');
+  }, 500);
+  setTimeout(() => {
+    msg.edit('🚬 ☁ ');
+  }, 1000);
+  setTimeout(() => {
+    msg.edit('🚬 ☁☁ ');
+  }, 1500);
+  setTimeout(() => {
+    msg.edit('🚬 ☁☁☁ ');
+  }, 2000);
+  setTimeout(() => {
+    msg.edit('🚬 ☁☁');
+  }, 2500);
+  setTimeout(() => {
+    msg.edit('🚬 ☁');
+  }, 3000);
+  setTimeout(() => {
+    msg.edit('🚬 ');
+  }, 3500);
+  setTimeout(() => {
+    msg.edit(`Finished smoking`);
+  }, 4000);
+});
+};
+}
+       
 })
 
 bot.login(process.env.BOT_TOKEN)
