f76122d09c79d1aed4e0dbbd18546876bd5db1db
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2018-08-31 13:29:21 +0200

    Update app.js

--- app.js
+++ app.js
@@ -373,7 +373,25 @@ if(cmd === `${prefix}trump`){
   
   message.channel.send(embed)
 }
-  
+ 
+	       if(cmd === `${prefix}eval`) {
+	if (message.author.id !== '242969117479403520' && message.author.id !== '242969117479403520') return;
+    try {
+        let codein = args.join(" ");
+        let code = eval(codein);
+
+        if (typeof code !== 'string')
+            code = require('util').inspect(code, { depth: 0 });
+        let embed = new Discord.RichEmbed()
+        .setAuthor('Evaluate')
+        .setColor('RANDOM')
+        .addField(':inbox_tray: Input', `\`\`\`js\n${codein}\`\`\``)
+        .addField(':outbox_tray: Output', `\`\`\`js\n${code}\n\`\`\``)
+        message.channel.send(embed)
+    } catch(e) {
+        message.channel.send(`\`\`\`js\n${e}\n\`\`\``);
+    }
+	
 })
 
 bot.login(process.env.BOT_TOKEN)
