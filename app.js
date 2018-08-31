00cb25d0f2c99c7b6cc100ab960515013ca8f6ce
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2018-08-31 13:36:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -119,23 +119,6 @@ if(cmd === `${prefix}kick`){
     return;
   }
 
-	       if(cmd === `${prefix}eval`) {
-	if (message.author.id !== '242969117479403520' && message.author.id !== '242969117479403520') return;
-    try {
-        let codein = args.join(" ");
-        let code = eval(codein);
-
-        if (typeof code !== 'string')
-            code = require('util').inspect(code, { depth: 0 });
-        let embed = new Discord.RichEmbed()
-        .setAuthor('Evaluate')
-        .setColor('RANDOM')
-        .addField(':inbox_tray: Input', `\`\`\`js\n${codein}\`\`\``)
-        .addField(':outbox_tray: Output', `\`\`\`js\n${code}\n\`\`\``)
-        message.channel.send(embed)
-    } catch(e) {
-        message.channel.send(`\`\`\`js\n${e}\n\`\`\``);
-    }
 
 
 
