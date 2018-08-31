a7fab2b2564efbf31ab9201673b8c252eb7f7ea4
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2018-08-31 23:03:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ bot.commands = new Discord.Collection()
 
 bot.on("ready", async () =>{
   console.log(`${bot.user.username} is online! It's running on ${bot.guilds.size} servers!`);
-  bot.user.setActivity("!pomoc", {type: "WATCHING"});
+  bot.user.setActivity("!pomoc", {type: "STREAMING"});
 })
 
 bot.on("message", async message =>{
