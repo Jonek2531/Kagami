9e3e32e04992f0a11e979e1fa3a56741593c6808
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2018-09-18 20:05:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -417,6 +417,9 @@ if(cmd === `${prefix}trump`){
 });
 };
 	
+	case 'say': if (message.startsWith("!say") == true) {//Check if the message send starts with "say" 
+		var newMessage = message.replace("say ", "");//Making a variable where "say " is removed 
+		bot.sendMessage({to: channelID, message:newMessage})//Send the new variable. } break;
 	
 if (cmd.startsWith(prefix + 'hug')) {
     let hug = [
