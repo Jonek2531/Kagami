e95dd4dd28bb88f6a45988f4402b1035d1634466
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2018-10-01 18:55:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ if (!message.member.roles.find("name", "@everyone")) { //Whatever role you want,
         .setColor("#ffffff") //To change color do .setcolor("#fffff")
         .setFooter('Aby zagłosować wystarczy wybrać odpowiednią reakcję.')
         .setDescription(args.join(' '))
-	.setTitle(`Ankieta!`);
+	.setTitle(`Ankieta od użytkownika ${message.author.username}`);
         
     if(cmd === `${prefix}ankieta`)
 	    message.channel.send(embed)
