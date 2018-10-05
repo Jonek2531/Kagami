af15928408dbebf827eba9103285ebc474fda62e
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2018-10-05 17:33:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -41,6 +41,14 @@ bot.on("message", async message =>{
     return message.channel.send(gayembed);
 };
 
+	    if (cmd === `${prefix}nick`) {
+		    
+   let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+   if(!kUser) return message.channel.send("Nie ma takiego użytkownika!");
+   let kReason = args.join(" ").slice(22);
+		    if(!message.member.hasPermission("MANAGE_NICKNAMES")) return message.channel.send("Nie możesz zmienić nicku tej osobie, ponieważ nie masz uprawnień!");
+   if(kUser.hasPermission("MANAGE_NICKNAMES")) return message.channel.send("Nie możesz zmienić nicku tej osoby!");
+        client.setNickname({nick: message.content.replace('changeNick ', '')});
 	
 	  if(cmd === `${prefix}zakończmójżywotpls`) {
     let user = message.author;
