d15c0d6a73265a0493a5d9071e86c75d1db6aec0
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2018-10-06 16:38:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -110,7 +110,7 @@ if (!message.member.roles.find("name", "@everyone")) { //Whatever role you want,
     message.channel.send(sayMessage);
   }
 
-if(cmd === `${prefix}kick`){
+if(cmd === `${prefix}kik`){
 
    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
    if(!kUser) return message.channel.send("Nie ma takiego użytkownika!");
@@ -135,7 +135,39 @@ if(cmd === `${prefix}kick`){
    kickChannel.send(kickEmbed);
 
    return;
+	
+
  }
+	  if(cmd === `${prefix}kick`) {
+    // This command must be limited to mods and admins. In this example we just hardcode the role names.
+    // Please read on Array.some() to understand this bit: 
+    // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
+    if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+    
+    // Let's first check if we have a member and if we can kick them!
+    // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
+    // We can also support getting the member by ID, which would be args[0]
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!member)
+      return message.reply("Please mention a valid member of this server");
+    if(!member.kickable) 
+      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+    
+    // slice(1) removes the first part, which here should be the user mention or ID
+    // join(' ') takes all the various parts to make it a single string.
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "No reason provided";
+    
+    // Now, time for a swift kick in the nuts!
+    await member.kick(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+
+  }
+  
+	
+	
  if(cmd === `${prefix}profil`) {
    let user = message.mentions.users.first() || message.author;
        let embed = new Discord.RichEmbed()
