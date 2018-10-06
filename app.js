58bbb66a6cb9a820c6dcc7193d0adfbd7c98b524
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2018-10-06 17:16:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -142,27 +142,41 @@ if(cmd === `${prefix}kik`){
     // This command must be limited to mods and admins. In this example we just hardcode the role names.
     // Please read on Array.some() to understand this bit: 
     // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
-    if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
+    if(!message.member.roles.some(r=>["HeadAdmin", "Administrator", "Moderator"].includes(r.name)) )
+      return message.reply("Sory ziomix, ale nie masz do tego uprawnień");
     
+		     let kickEmbed = new Discord.RichEmbed()
+   .setDescription("Kick")
+   .setColor("#00e7ff")
+   .addField("Wyrzucony użytkownik", `${kUser}, ID użytkownika ${kUser.id}`)
+   .addField("Wyrzucony przez", `<@${message.author.id}>, ID użytkownika: ${message.author.id}`)
+   .addField("Na kanale", message.channel)
+   .addField("O godzinie", message.createdAt)
+   .addField("Powód", kReason);
+
+   let kickChannel = message.guild.channels.find(`name`, "logi");
+   if(!kickChannel) return message.channel.send("Nie mogę znaleść danego kanału.");
+
+
+   kickChannel.send(kickEmbed);
     // Let's first check if we have a member and if we can kick them!
     // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
     // We can also support getting the member by ID, which would be args[0]
     let member = message.mentions.members.first() || message.guild.members.get(args[0]);
     if(!member)
-      return message.reply("Please mention a valid member of this server");
+      return message.reply("Oznacz osobę którą mam wyrzucić");
     if(!member.kickable) 
-      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+      return message.reply("Niestety, ale nie mogę wyrzucić osoby która ma rolę wyższą ode mnie -,-");
     
     // slice(1) removes the first part, which here should be the user mention or ID
     // join(' ') takes all the various parts to make it a single string.
     let reason = args.slice(1).join(' ');
-    if(!reason) reason = "No reason provided";
+    if(!reason) reason = "Nie podano powodu";
     
     // Now, time for a swift kick in the nuts!
     await member.kick(reason)
       .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+    message.reply(`${member.user.tag}  ${message.author.tag} because: ${reason}`);
 
   }
   
