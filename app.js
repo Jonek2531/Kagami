95d5dc9d2986833ec38c6ee087fccb68880bfd16
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2018-12-03 16:40:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Nie mas
 let dMessage = args.join(" ").slice(22);
 if(dMessage.length < 1) return message.reply('Musisz napisać coś w wiadomości!')
 
-dUser.send(`Masz nową wiadomość z serwera GamerStay. Oto treść wiadomości: ${dMessage}.`)
+dUser.send(`${dMessage}`)
 
 }
   if (cmd === `${prefix}info`) {
