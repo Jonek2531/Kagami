b1208e9d3aec5484b702e0e99e988de42135de3c
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2018-12-31 10:04:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ dUser.send(`${dMessage}`)
 	
 
   if (cmd === `${prefix}przypomnienierang`){
-    message.channel.send("Możliwe rangi do dodania sobie: ?fortnite, ?minecraft, ?csgo, ?gmod, ?tf2, ?overwatch, ?lol, ?paladins, ?isaac, ?roblox, ?dbd, ?giveaway, ?furry, ?nsfw, ?tyralnia, ?osu, ?rocketleague, ?rust, ?subnautica, ?projectzomboid, ?pathofexile, ?simsy, ?geometrydash, ?pubg, ?gta, ?eu, ?scrapmechanic, ?townofsalem, ?quake.");
+    message.channel.send("Możliwe rangi do dodania sobie: ?fortnite, ?minecraft, ?csgo, ?gmod, ?tf2, ?overwatch, ?lol, ?paladins, ?isaac, ?roblox, ?dbd, ?giveaway, ?furry, ?nsfw, ?tyralnia, ?osu, ?rocketleague, ?rust, ?subnautica, ?projectzomboid, ?pathofexile, ?simsy, ?geometrydash, ?pubg, ?gta, ?eu, ?scrapmechanic, ?townofsalem, ?quake, ?brawlhalla, ?qotd, ?clashroyale, ?theforest, ?battlefield, ?r6s.");
   }
 
   if(cmd === `${prefix}avatar`) {
