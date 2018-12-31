13253639d4440df6d78c68c0940ae164de5395dc
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2018-12-31 10:06:36 +0100

    Update app.js

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ dUser.send(`${dMessage}`)
 	
 	
 
-  if (cmd === `${prefix}przypomnienierang`){
+  if (cmd === `${prefix}rangi`){
     message.channel.send("Możliwe rangi do dodania sobie: ?fortnite, ?minecraft, ?csgo, ?gmod, ?tf2, ?overwatch, ?lol, ?paladins, ?isaac, ?roblox, ?dbd, ?giveaway, ?furry, ?nsfw, ?tyralnia, ?osu, ?rocketleague, ?rust, ?subnautica, ?projectzomboid, ?pathofexile, ?simsy, ?geometrydash, ?pubg, ?gta, ?eu, ?scrapmechanic, ?townofsalem, ?quake, ?brawlhalla, ?qotd, ?clashroyale, ?theforest, ?battlefield, ?r6s.");
   }
 
