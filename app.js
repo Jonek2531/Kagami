be3848b3cb784e671b3744378849c6ff296cc11b
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2019-03-07 18:13:01 +0100

    Update app.js

--- app.js
+++ app.js
@@ -30,10 +30,10 @@ bot.on("message", async message =>{
     .setDescription("Administracja Serwera")
     .setColor("RANDOM")
     .setTimestamp(new Date())
-    .addField("HeadAdministratorzy", "<@329694416472375298>, <@306150715678654464>, <@287999995230027787>, <@348468240520970260>")
-    .addField("Administratorzy", "<@273427160835031040>,<@301263420471246848>, <@470312950213902356>, <@352156710074449920>, <@336576701414899735>")
-    .addField("Moderatorzy", "<@318664079969353740>")
-    .addField("Helperzy", "<@351722490487373834>")
+    .addField("HeadAdministratorzy", "<@329694416472375298>, <@306150715678654464>, <@287999995230027787>, <@348468240520970260>, <@301263420471246848>")
+    .addField("Administratorzy", "ne ma :((")
+    .addField("Moderatorzy", "<@470312950213902356>, <@351722490487373834>")
+    .addField("Helperzy", "<@336755369667723276>")
     return message.channel.send(admembed);
   }
 	
