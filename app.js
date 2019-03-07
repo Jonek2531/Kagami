1da3aadf17a2bced0bac3bf783be0850bd00ec53
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2019-03-07 22:24:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const botconfig = require("./botconfig.json");
 const fs = require("fs");
 const client = new Discord.Client();
 const bot = new Discord.Client({disableEveryone: true});
-emitter.setMaxListeners()
+
 bot.commands = new Discord.Collection()
 
 bot.on("ready", async () =>{
