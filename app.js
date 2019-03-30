e9260e84fe1d5d4f3229e6730a38f700003479b0
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2019-03-30 12:48:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -239,6 +239,7 @@ let user = message.mentions.users.first() || message.author;
 	
 	
 	 if(cmd === `${prefix}say`) {
+if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Nie masz uprawnień do korzystania z tej komendy.");
     // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
     // To get the "message" itself we join the `args` back into a string with spaces: 
     const sayMessage = args.join(" ");
