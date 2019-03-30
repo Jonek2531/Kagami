2814edaedcf1c1b097694a215161dcee7b160069
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2019-03-30 16:15:10 +0100

    Update app.js

--- app.js
+++ app.js
@@ -357,13 +357,6 @@ if(cmd === `${prefix}kik`){
     return;
   }
 
-if(cmd === `${prefix}serwer`){
-message.channel.send("Witaj w menu informacji o serwerze. Jeśli chcesz zobaczyć wszystkie informacje o serwerze, to wpisz !serverinfo, jeśli chcesz tylko nazwę serwera to wyślij emotkę :one:, jeśli chcesz zobaczyć liczbę członków to wyślij emotkę :two:.");
-}
-	
-	if (message.content === 'asd') {
-		message.channel.send(`Nazwa serwera: **${message.guild.name}**`);
-}
 
   if(cmd === `${prefix}serverinfo`){
 
