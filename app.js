16702d2e9630d12207f062e3ad68382f2637cc21
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2019-04-12 13:02:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -120,7 +120,7 @@ channel.send('Propozycja:\n' + args.join(' '))
              .then(function (msg) {
             msg.react("❎");
             msg.react("✅"); // You can only add two reacts
-            message.delete({timeout: 1000});
+      
             })
 }     //Closes the if (command === 'suggest'){ 
 		      	
