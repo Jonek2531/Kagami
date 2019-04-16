10fb8d8e955f5c4fad83b3fef6479c7e2e057c6d
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2019-04-16 17:54:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -223,7 +223,7 @@ let user = message.mentions.users.first() || message.author;
                  .setThumbnail(`${user.avatarURL}`)
                  .addField("Nazwa Użytkownika", `${user.username}#${user.discriminator} (ID: ${user.id})` , inline = true)
                  .addField("Utworzono dnia", `${user.createdAt}`)      
-       .addField("Status", `${user.presence.status}, w grze **${user.presence.status}**`)
+       .addField("Status", `${user.presence.status}, w grze **${user.presence.game}**`)
                         .addField("Najnowsza wiadomość", `${user.lastMessage} (ID: ${channel.lastMessageID})`);
             return message.channel.send(embed);
         }
