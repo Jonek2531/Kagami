ac5168bb8de823477fa5baf94c370741a6437e18
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2019-04-30 17:31:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -313,7 +313,7 @@ let user = message.mentions.users.first() || message.author;
 			if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Nie masz uprawnień do korzystania z tej komendy.");
     	message.channel.send("Czy jesteś pewien, że chcesz pingnąć rolę Giveaway? Napisz 'TAK' jeśli tak lub 'NIE' jeśli nie.");
 			if(message.content === "TAK"){
-message.channel.send('<@&476766899611369483> aby wziąć udział należy zareagować reakcją 🎉')
+message.channel.send('<@&476766899611369483> aby wziąć udział należy zareagować reakcją 🎉');
 		 if(message.content === "NIE")
 					message.channel.send("Anulowano.");
 			}
