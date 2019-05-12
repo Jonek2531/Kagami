44f89828632f3ea0e2aac2c81a5a8659fba94308
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2019-05-12 17:11:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -44,25 +44,6 @@ let embed = new Discord.RichEmbed()
     .setDescription(`**Monthly Reward**`)
     .addField(`Collected`, "liczba")
 
-if(cmd === `${prefix}testprofil1`){
-let user = message.mentions.users.first() || message.author;
-    const joinDiscord = moment(user.createdAt).format('llll');
-    const joinServer = moment(user.joinedAt).format('llll');
-    let embed = new Discord.RichEmbed()
-        .setAuthor(user.username + '#' + user.discriminator, user.displayAvatarURL)
-        .setDescription(`${user}`)
-        .setColor(`RANDOM`)
-        .setThumbnail(`${user.displayAvatarURL}`)
-        .addField('Joined at:', `${moment.utc(user.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
-        .addField('Status:', user.presence.status, true)
-        .addField('Roles:', user.roles.map(r => `${r}`).join(' | '), true)
-        .setFooter(`ID: ${user.id}`)
-        .setTimestamp();
-
-    message.channel.send(embed);
-    return;
-}
-message.channel.send(embed);
 if (message.content.includes("discord.gg")) {
     message.delete(1);
     message.channel.sendMessage("Nie reklamuj serwerów discord tutaj, od tego są <#449282771714572291>, " + message.author)
