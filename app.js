4dfaaa8608edbea9687918ee8e47d209d90a8e12
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2019-06-10 16:12:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -565,7 +565,6 @@ if(cmd === `${prefix}kik`){
     return;
   }
 
-
   if(cmd === `${prefix}serverinfo`){
 
     let serverembed = new Discord.RichEmbed()
@@ -580,7 +579,7 @@ if(cmd === `${prefix}kik`){
     .addField("Liczba użytkowników", message.guild.memberCount, true)
     .addField("Poziom potwierdzenia konta", message.guild.verificationLevel, true)
         .addField("Data stworzenia serwera", `${moment.utc(message.guild.createdAt).format('dddd, MMMM Do YYYY, HH:mm:ss')}`, true)
-    .setThumbnail(`${message.author.avatarURL}`);
+    .setThumbnail(`${message.guild.avatarURL}`);
 
 
     return message.channel.send(serverembed);
