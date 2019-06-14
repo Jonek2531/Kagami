df4d11c09c4af41672dcb8bf328300aba27cbd3c
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2019-06-14 22:47:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -391,7 +391,7 @@ if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send
 let user = message.mentions.users.first() || message.author;
 		 let channel = message.mentions.users.first() || message.author;
 		 let gra = "w grze " + user.presence.game;
-		 if(user.presence.game === "w grze Spotify") user.presence.game = "słucha muzyki na Spotify";
+		 if(gra === "w grze Spotify") gra = "słucha muzyki na Spotify";
 		 if(gra === "w grze " + null) gra = "aktualnie nie jest w grze";
        let embed = new Discord.RichEmbed()
                  .setTitle("Profil")
