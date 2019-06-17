050508efebc021dbfc2c98e420dab55f5dbb3073
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2019-06-17 13:30:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -40,22 +40,22 @@ let userData = JSON.parse(fs.readFileSync('Storage/userData.json', 'utf8'));
 	  let banned = message.author
 	  let banchannel = message.guild.channels.find(ch => ch.name === 'logi');
     message.delete(1);
-    message.channel.sendMessage("W tej chwili został zbanowany użytkownik, za wysyłanie linków do nieodpowiednich stron. Nazwa --> " + message.author + message.author.nickname)
-	  message.banchannel.member(banned).ban("sending nudes - użytkownik automatycznie zbanowany przez bota")	  
+    message.channel.sendMessage("W tej chwili został zbanowany użytkownik, za wysyłanie linków do nieodpowiednich stron. Nazwa --> " + message.author)
+	  message.guild.banchannel.member(banned).ban("sending nudes - użytkownik automatycznie zbanowany przez bota")	  
   }
 	 if (message.content.includes("privatepage.vip")) {
 	  let banned = message.author
 	  let banchannel = message.guild.channels.find(ch => ch.name === 'logi');
     message.delete(1);
-    message.channel.sendMessage("W tej chwili został zbanowany użytkownik, za wysyłanie linków do nieodpowiednich stron. Ping --> " + message.author + message.author.nickname)
-	  message.banchannel.member(banned).ban("sending nudes - użytkownik automatycznie zbanowany przez bota")	  
+    message.channel.sendMessage("W tej chwili został zbanowany użytkownik, za wysyłanie linków do nieodpowiednich stron. Ping --> " + message.author)
+	  message.guild.banchannel.member(banned).ban("sending nudes - użytkownik automatycznie zbanowany przez bota")	  
   }
 	  if (message.content.includes("viewc.site")) {
 	  let banned = message.author
 	  let banchannel = message.guild.channels.find(ch => ch.name === 'logi');
     message.delete(1);
-    message.channel.sendMessage("W tej chwili został zbanowany użytkownik, za wysyłanie linków do nieodpowiednich stron. Nazwa --> " + message.author + message.author.nickname)
-	  message.banchannel.member(banned).ban("sending nudes - użytkownik automatycznie zbanowany przez bota")	  
+    message.channel.sendMessage("W tej chwili został zbanowany użytkownik, za wysyłanie linków do nieodpowiednich stron. Nazwa --> " + message.author)
+	  message.guild.banchannel.member(banned).ban("sending nudes - użytkownik automatycznie zbanowany przez bota")	  
 	  }
 	
 	  if(cmd === `${prefix}administracja`){
