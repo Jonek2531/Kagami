9b318f328de88679c8a071c7500f0bb75a2aacbc
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2019-06-17 16:54:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -223,61 +223,6 @@ let dMessage = args.join(" ").slice(22);
 if(dMessage.length < 1) return message.reply('Musisz napisać coś w wiadomości!')
 
 dUser.send(`${dMessage}`)
-
-}
-	
-	const Command = require('../../Storage/Command');
-
-class ClientStats extends Command {
-    constructor() {
-        super();
-        this.help = {
-            name: 'clientstats',
-            category: 'admin',
-            description: 'Get detailed statistics about the bot',
-            usage: '{prefix}stats'
-        };
-        this.conf = {
-            requireDB: false,
-            disabled: false,
-            aliases: ["cs", "botstats"],
-            requirePerms: [],
-            guildOnly: false,
-            ownerOnly: false,
-            expectedArgs: []
-        };
-    }
-
-    async run(client, message) {
-        message.channel.createMessage({
-            embed: {
-                title: ':gear: Client stats',
-                fields: [{
-                        name: 'Clusters',
-                        value: `Total: ${client.stats.clusters.length}\nActive: ${client.stats.clusters.length - client.stats.clusters.filter(c => c.guilds < 1).length}`,
-                        inline: true
-                    },
-                    {
-                        name: 'RAM usage',
-                        value: `${client.stats.totalRam.toFixed(2)}MB`,
-                        inline: true
-                    },
-                    {
-                        name: 'General stats',
-                        value: `Guilds: ${client.stats.guilds} | Cached users: ${client.stats.users} | Large guilds: ${client.stats.largeGuilds}`
-                    },
-                    {
-                        name: 'Clusters stats',
-                        value: '```' + client.stats.clusters.map(c => `Cluster ${c.cluster}: ${c.shards} shard(s) | ${c.guilds} guild(s) | ${c.ram.toFixed(2)}MB RAM used | Up for ${client.timeConverter.toElapsedTime(c.uptime, true)}`).join('\n--\n') + '```'
-                    }
-                ]
-            },
-            color: client.config.options.embedColor
-        });
-    }
-}
-
-module.exports = new ClientStats();
 	
 	if(cmd === `${prefix}ustaw-awatar`) {
     let messageArray = message.content.split(" ");
