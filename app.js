b9e2bc7876140acd05bb448dcf4a2580db6b30ac
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2019-07-02 12:49:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ bot.commands = new Discord.Collection()
 //l
 bot.on("ready", async () =>{
   console.log(`${bot.user.username} is online! It's running on ${bot.guilds.size} servers!`);
-  bot.user.setActivity(`${guild.members.filter(member => !member.user.bot).size} kociaków ❤️, {type: "WATCHING"}`);
+  bot.user.setActivity("!pomoc", {type: "WATCHING"}`);
 })
 
 bot.on("message", async message =>{
