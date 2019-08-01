cb4f33e0f8fa16f729bdd7afeb59467783b318e5
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2019-08-01 20:41:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -187,7 +187,7 @@ member.addRole(role).catch(console.error)
 		if(cmd === `${prefix}werinfo`){
 		let werembed = new Discord.RichEmbed()
 		        .setTitle(`Weryfikacja`)
-        .addField('Witaj na serwerze!', 'Znajdujesz się aktualnie w strefie weryfikacji? Weryfikacja została stworzona po to, aby zabezpieczyć serwer przez potencjalnymi zagrożeniami.')
+        .addField('Witaj na serwerze!', 'Znajdujesz się aktualnie w strefie weryfikacji. Weryfikacja została stworzona po to, aby zabezpieczyć serwer przez potencjalnymi zagrożeniami.')
 		.addField('Jak się zweryfikować?', 'Aby się zweryfikować, zaznacz __**DWUKROTNIE**__ reakcję <:Dude:518909814248701993> pod wiadomością wyżej.', true);
 		return message.channel.send(werembed)
 	}
