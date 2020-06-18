a537c8eeef2619388a47e2d4a014ba613f367ca2
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2020-06-19 00:01:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -90,25 +90,25 @@ dUser.send(`${dMessage}`)
 		message.channel.send(`${message.author} ungimped ${member}`);
 	}
 
-		let gimped = [
-	"SCP-049 'leczy' w zombie...",
-	"SCP-049-2 to przydupas SCP-049...",
-	"SCP-066 - można powiedzieć, że to coś powoduje tak jakby 'trzęsienie ziemi'...",
-	"SCP-082 jest grubszy niż Personel Klasy D-1...",
-	"SCP-096 jest strasznie nieśmiały...",
-	"SCP-106 robi cz@rne plamy...",
-	"SCP-173 lubi skręcać karki...",
-	"SCP-372 bardzo lubi przyczepiać się o ściany...",
-	"SCP-457 jest hot, bo się naoglądał... (wtf)...",
-	"SCP-527 to zdrajca drugiej generacji...",
-	"SCP-939 właściwie to jest ślepy...",
-        "SCP-999 to zdrajca pierwszej generacji...",
-	"SCP-1048a strasznie krzyczy, nieprawdaż?...",
-			]
-			let gimpedresult = Math.floor((Math.random() * gimped.length));
-       if(message.member.roles.has(723265073241391126)) {
-	       message.channel.send(gimpedresult)
-}
+	//	let gimped = [
+//	"SCP-049 'leczy' w zombie...",
+	//"SCP-049-2 to przydupas SCP-049...",
+	//"SCP-066 - można powiedzieć, że to coś powoduje tak jakby 'trzęsienie ziemi'...",
+	//"SCP-082 jest grubszy niż Personel Klasy D-1...",
+	//"SCP-096 jest strasznie nieśmiały...",
+	//"SCP-106 robi cz@rne plamy...",
+	//"SCP-173 lubi skręcać karki...",
+	//"SCP-372 bardzo lubi przyczepiać się o ściany...",
+	//"SCP-457 jest hot, bo się naoglądał... (wtf)...",
+	//"SCP-527 to zdrajca drugiej generacji...",
+	//"SCP-939 właściwie to jest ślepy...",
+      //  "SCP-999 to zdrajca pierwszej generacji...",
+	//"SCP-1048a strasznie krzyczy, nieprawdaż?...",
+			//]
+			//let gimpedresult = Math.floor((Math.random() * gimped.length));
+      // if(message.member.roles.has(723265073241391126)) {
+	       //message.channel.send(gimpedresult)
+//}
 	
 if(cmd === `${prefix}opusc`){
 	if(message.author.id !=='329694416472375298') 
