2b756290ae61319a7d6f1de0b6561d20723c876c
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2020-08-27 20:16:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -28,6 +28,11 @@ bot.on('guildMemberAdd', member => {
  let lobby = bot.channels.get("712286891902500877");
 	member.guild.channels.get('712286891902500877').send("Witaj " + member.user.username + "!\n**●** Na samym początku zapoznaj się z <#747054073487818782>, ponieważ znajdują się tam zasady, które należy przestrzegać grając na serwerze.\n**●** Następnie przeczytaj <#712299665827299386>, aby wiedzieć jak funkcjonuje nasz serwer na discordzie.\n**●** Gdy zapoznasz się z obydwona regulaminami, obczaj sobie te kanały: <#747055160173264896> oraz <#747444965549932564>, gdyż mogą one się przyczynić do lepszej znajomości chociażby mapy albo zasad działania klas.\n**●** Jesteś zainteresowany kupnem VIPa? Zapraszamy na kanał <#739248187922972732>.\n**●** Zostałaś/eś oprowadzona/y przez podstawowe kanały serwera. Zachęcamy do zapoznania się z pozostałymi kanałami, aktywnym udzielaniu się na serwerze, zakładania własnych sugestii dotyczących trybu, zgłaszania wszelkich wykrytych błędów oraz miłego spędzania czasu na naszym discordzie. <:3_:714210125149241407>");
 });
+
+bot.on('guildMemberRemove', member => {
+ let lobby = bot.channels.get("712286891902500877");
+	member.guild.channels.get('712286891902500877').send(member.user.username + " postanowił/a nas opuścić.");
+});
 bot.on("messageDelete", async message =>{
   let LoggingEmbed = new Discord.RichEmbed()
 .setTitle("BREAKING NEWS! Wiadomość została usunięta!")
