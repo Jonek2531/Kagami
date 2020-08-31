15f71a82e85d1ea4a2e8551fd8313a194405fbda
Jonek2531 <42818118+Jonek2531@users.noreply.github.com>
2020-08-31 12:10:55 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,15 @@ const ms = require('ms')
 const botconfig = require("./botconfig.json");
 const fs = require("fs")
 const client = new Discord.Client();
+const CronJob = require('cron').CronJob;
+const query = require("source-server-query");
+const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection()
+let rawdata = fs.readFileSync('config.json');
+let config = JSON.parse(rawdata);
+var change_json = false;
+
 
 	bot.on("ready", async () =>{
   console.log(`${bot.user.username} jest online! Aktualnie jest na ${bot.guilds.size} serwerach.`);
@@ -25,10 +32,8 @@ bot.on('guildMemberAdd', member => {
 	onlinevc.setName("Członkowie: " + onlineCount);
 });
 bot.on('guildMemberAdd', member => {
-	let gamerstay = bot.guilds.get('712286891902500874');
-	var onlineCount = gamerstay.members.filter(m => m.presence.status === 'online').size + gamerstay.members.filter(m => m.presence.status === 'idle').size + gamerstay.members.filter(m => m.presence.status === 'dnd').size + gamerstay.members.filter(m => m.presence.status === 'offline').size
  let lobby = bot.channels.get("712286891902500877");
-	member.guild.channels.get('712286891902500877').send("Witaj <@" + member.user.id + ">!\n**●** Na samym początku zapoznaj się z <#747054073487818782>, ponieważ znajdują się tam zasady, które należy przestrzegać grając na serwerze.\n**●** Następnie przeczytaj <#712299665827299386>, aby wiedzieć jak funkcjonuje nasz serwer na discordzie.\n**●** Gdy zapoznasz się z obydwoma regulaminami, obczaj sobie te kanały: <#747055160173264896> oraz <#747444965549932564>, gdyż mogą one się przyczynić do lepszej znajomości chociażby mapy albo zasad działania klas.\n**●** Jesteś zainteresowany kupnem VIPa? Zapraszamy na kanał <#739248187922972732>.\n**●** Zostałaś/eś oprowadzona/y przez podstawowe kanały serwera. Zachęcamy do zapoznania się z pozostałymi kanałami, aktywnym udzielaniu się na serwerze, zakładania własnych sugestii dotyczących trybu, zgłaszania wszelkich wykrytych błędów oraz miłego spędzania czasu na naszym discordzie. <:SCP173:748613466629341358>\n\nJesteś " + onlineCount + " użytkownikiem serwera.");
+	member.guild.channels.get('712286891902500877').send("Witaj <@" + member.user.id + ">!\n**●** Na samym początku zapoznaj się z <#747054073487818782>, ponieważ znajdują się tam zasady, które należy przestrzegać grając na serwerze.\n**●** Następnie przeczytaj <#712299665827299386>, aby wiedzieć jak funkcjonuje nasz serwer na discordzie.\n**●** Gdy zapoznasz się z obydwoma regulaminami, obczaj sobie te kanały: <#747055160173264896> oraz <#747444965549932564>, gdyż mogą one się przyczynić do lepszej znajomości chociażby mapy albo zasad działania klas.\n**●** Jesteś zainteresowany kupnem VIPa? Zapraszamy na kanał <#739248187922972732>.\n**●** Zostałaś/eś oprowadzona/y przez podstawowe kanały serwera. Zachęcamy do zapoznania się z pozostałymi kanałami, aktywnym udzielaniu się na serwerze, zakładania własnych sugestii dotyczących trybu, zgłaszania wszelkich wykrytych błędów oraz miłego spędzania czasu na naszym discordzie. <:SCP173:748613466629341358>");
 member.addRole('712294398360027177');
 });
 
@@ -252,6 +257,146 @@ if(cmd === `${prefix}opusc`){
     var guildID = bot.guild.find(493320672844447745)
     guildID.leave()
 }
+	
+	client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    var guild = client.guilds.cache.get(config.discord_server_id);
+    if (!guild) return console.log('Unable to find guild. Check config.json');
+
+    for (let key in config.servers) {
+        var server = config.servers[key];
+
+        if (config.showname) {
+            if (server.channel_name == "") {
+                change_json = true;
+                guild.channels.create('channel_name', {
+                    type: 'voice',
+                    permissionOverwrites: [{
+                        id: guild.roles.everyone.id,
+                        deny: ['CONNECT'],
+                    }],
+                }).then(result => {
+                    config.servers[key].channel_name = result.id
+                });
+
+            }
+        }
+        if (config.showmap) {
+            if (server.channel_map == "") {
+                change_json = true;
+                guild.channels.create('channel_map', {
+                    type: 'voice',
+                    permissionOverwrites: [{
+                        id: guild.roles.everyone.id,
+                        deny: ['CONNECT'],
+                    }],
+                }).then(result => {
+                    config.servers[key].channel_map = result.id
+                });
+
+            }
+        }
+        if (config.showgamemode) {
+            if (server.channel_gamemode == "") {
+                change_json = true;
+                guild.channels.create('channel_gamemode', {
+                    type: 'voice',
+                    permissionOverwrites: [{
+                        id: guild.roles.everyone.id,
+                        deny: ['CONNECT'],
+                    }],
+                }).then(result => {
+                    config.servers[key].channel_gamemode = result.id
+                });
+
+            }
+        }
+        if (config.showplayers) {
+            if (server.channel_players == "") {
+                change_json = true;
+                guild.channels.create('channel_players', {
+                    type: 'voice',
+                    permissionOverwrites: [{
+                        id: guild.roles.everyone.id,
+                        deny: ['CONNECT'],
+                    }],
+                }).then(result => {
+                    config.servers[key].channel_players = result.id
+                });
+
+            }
+        }
+    }
+    if (change_json) {
+        setTimeout(function() {
+            fs.writeFile("config.json", JSON.stringify(config, null, 2), function(err) {
+                if (err) throw err;
+                console.log("Settings SAVED");
+                job.start();
+            });
+
+        }, 3000);
+    } else {
+       job.start();
+    }
+
+});
+
+function parse(str) {
+    var args = [].slice.call(arguments, 1),
+        i = 0;
+
+    return str.replace(/%s/g, () => args[i++]);
+}
+
+async function update_server_info(server, info) {
+    console.log('Updating channels info');
+    //must add extra check here
+    try {
+        if (config.showname) {
+            var channel = client.channels.cache.get(server.channel_name);
+            channel.setName(parse(config.text_name, info.name));
+        }
+        if (config.text_map) {
+            var channel = client.channels.cache.get(server.channel_map);
+            channel.setName(parse(config.text_map, info.map));
+        }
+        if (config.showgamemode) {
+            var channel = client.channels.cache.get(server.channel_gamemode);
+            channel.setName(parse(config.text_gamemode, info.game));
+        }
+        if (config.showplayers) {
+            var channel = client.channels.cache.get(server.channel_players)
+            channel.setName(parse(config.text_players, info.playersnum, info.maxplayers));
+        }
+        console.log("done");
+    } catch (err) {
+        console.log(err);
+    }
+
+}
+var job = new CronJob('* * * * *', function() {
+    
+        for (let server of config.servers) {
+            let ip = server.ip;
+            let port = server.port;
+            console.log('Starting quering server ' + ip + ':' + port);
+            query
+                .info(ip, port, config.query_timeout)
+                .then(info => {
+                    update_server_info(server, info)
+                }) //success
+
+                .catch(console.log); //error
+
+
+        }
+
+    
+
+}, null, true, 'Europe/Warsaw');
+	
+	
 })
 
 bot.login(process.env.BOT_TOKEN)
